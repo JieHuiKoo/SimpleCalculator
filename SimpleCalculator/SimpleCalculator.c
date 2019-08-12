19bd6eb5daee55afa9436f9a6749eabbe7beb8af
Koo Jie Hui <koojiehui@outlook.com>
2019-08-12 19:43:51 +0800

    Added +. -, *, /, sqrt, ^ functionality

--- SimpleCalculator/SimpleCalculator.c
+++ SimpleCalculator/SimpleCalculator.c
@@ -6,7 +6,92 @@
 
 int main()
 {
-	printf("Hello\n");
+	int exit_choice = 1;
+	int input_error = 0;
+	double first_number, second_number;
+
+	char c;
+
+	while (exit_choice)
+	{
+		int int_operand_choice = 0;
+	
+		printf("\n\n\n\n\n=====Welcome to the simple calculator app!=====\n\n");
+
+		do 
+		{
+			input_error = 0;
+			printf("Please select operand:\n1) +\n2) -\n3) *\n4) /\n5)sqrt\n6)power\n\nChoice: ");
+			scanf_s("%d", &int_operand_choice, 1);
+			printf("\n");
+			while ((c = getchar()) != '\n' && c != EOF);
+			
+ 			switch (int_operand_choice)
+			{
+			case 1:
+				printf("=== You selected + ===\n\nPlease key in the first number: ");
+				scanf_s("%lf", &first_number, 1);
+				while ((c = getchar()) != '\n' && c != EOF);
+				printf("\nPlease key in the second number: ");
+				scanf_s("%lf", &second_number, 1);
+				while ((c = getchar()) != '\n' && c != EOF);
+				printf("\n%lf + %lf = %lf\n\n", first_number, second_number, first_number + second_number);
+				break;
+			case 2:
+				printf("=== You selected - ===\n\nPlease key in the first number: ");
+				scanf_s("%lf", &first_number, 1);
+				while ((c = getchar()) != '\n' && c != EOF);
+				printf("\nPlease key in the second number: ");
+				scanf_s("%lf", &second_number, 1);
+				while ((c = getchar()) != '\n' && c != EOF);
+				printf("\n%lf - %lf = %lf\n\n", first_number, second_number, first_number - second_number);
+				break;
+			case 3:
+				printf("=== You selected * ===\n\nPlease key in the first number: ");
+				scanf_s("%lf", &first_number, 1);
+				while ((c = getchar()) != '\n' && c != EOF);
+				printf("\nPlease key in the second number: ");
+				scanf_s("%lf", &second_number, 1);
+				while ((c = getchar()) != '\n' && c != EOF);
+				printf("\n%lf * %lf = %lf\n\n", first_number, second_number, first_number * second_number);
+				break;
+			case 4:
+				printf("=== You selected / ===\n\nPlease key in the first number: ");
+				scanf_s("%lf", &first_number, 1);
+				while ((c = getchar()) != '\n' && c != EOF);
+				printf("\nPlease key in the second number: ");
+				scanf_s("%lf", &second_number, 1);
+				while ((c = getchar()) != '\n' && c != EOF);
+				printf("%lf / %lf = %lf\n\n", first_number, second_number, first_number / second_number);
+				break;
+			case 5:
+				printf("=== You selected root ===\n\nPlease key in the number to be square rooted");
+				scanf_s("%lf", &first_number, 1);
+				while ((c = getchar()) != '\n' && c != EOF);
+				printf("\nsquare root of %lf = %lf\n\n", first_number, sqrt(first_number));
+				break;
+			case 6:
+				printf("=== You selected power ===\n\nPlease key in the exponent: ");
+				scanf_s("%lf", &first_number, 1);
+				while ((c = getchar()) != '\n' && c != EOF);
+				printf("\nPlease key in the number: ");
+				scanf_s("%lf", &second_number, 1);
+				while ((c = getchar()) != '\n' && c != EOF);
+				printf("\n%lf^%lf = %lf\n\n", second_number, first_number, pow(first_number,second_number));
+				break;
+				break;
+			default:
+				printf("=== Invalid operand choice! Please try again. ===\n\n");
+				input_error = 1;
+				break;
+			}
+		} while (input_error);
+
+		printf("Do you want to continue?\n1) No\n2) Yes\nChoice: ");
+		scanf_s("%d", &exit_choice, 1);
+		while ((c = getchar()) != '\n' && c != EOF);
+		exit_choice--;		
+	}
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
